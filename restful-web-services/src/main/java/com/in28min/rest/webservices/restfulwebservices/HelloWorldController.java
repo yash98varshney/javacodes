bbd99a68ee2945de9652eac63b183fe939cc6e14
Yash <yash@gmail.com>
2023-08-02 19:34:42 +0530

    Adding internationlization

--- restful-web-services/src/main/java/com/in28min/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28min/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,25 @@
 package com.in28min.rest.webservices.restfulwebservices;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
 
+	private MessageSource messagesource; 
+	
+	public HelloWorldController(MessageSource messagesource){
+		this.messagesource=messagesource;
+	}
+	
+	@GetMapping("/hello-world-internationalization")
+	public String internationalization() {
+		
+		Locale locale=LocaleContextHolder.getLocale();
+		return messagesource.getMessage("local.language.message", null, "Default Message", locale);
+	}
 }
